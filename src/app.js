f4f9ebddfe06937eeca0b8b7af73938d7d7b1bdd
ejeh <ejehgodfrey@gmail.com>
2019-11-24 12:53:10 +0100

    prepared for deploment

--- src/app.js
+++ src/app.js
@@ -67,7 +67,7 @@ app.use("/api/v1", api);
 // Serve as static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/bulid"));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
